fde3ebf0199ab7be0cc1ddb048cacb2a32248c0a
ninest <parth.kabra@gmail.com>
2020-05-26 22:17:13 +0800

    added static generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,5 +29,18 @@ export default {
     scss: [
       'assets/styles/screen.scss',
     ]
+  },
+  generate: {
+    async routes () {
+      // next comment to make VSCode ignore the "error"
+      // @ts-ignore
+      const { $content } = require('@nuxt/content')
+      const pages = await $content().only(['path']).fetch()
+      const posts = await $content('posts').only(['path']).fetch()
+      
+      const files = [...pages,...posts,]
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
   }
 }
